33f1e5324eded40f55efa1f38829de3cd79b2b1e
Louis <info@louisdev.de>
2023-06-11 02:02:01 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,77 @@
-# SatelliteFreqMon
-Open source solution for accurately monitoring satellite passes using configurable TLE and precise frequency recording using the software-defined radio application GQRX
+# Automated Satellite SDR Groundstation
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+[![Python](https://img.shields.io/badge/python-3.8-blue.svg)](https://www.python.org/downloads/release/python-380/)
+
+The goal of this project is to provide a comprehensive open source solution for accurately monitoring satellite passes[^1] using configurable [TLE (Two-Line Elements)](https://en.wikipedia.org/wiki/Two-line_element_set)[^2] and precise frequency recording using the software-defined radio application GQRX[^3].
+
+
+## Hardware Setup
+
+<img src="git-assets/hardware-setup.png?raw=true" width="70%">
+
+### Hardware List
+- RTL-SDR RTL2832U Dongle
+- Wideband LNA
+- V Dipole Antenna (tuned for NOAA frequencies)
+- Raspberry Pi
+
+
+## TLE File Configuration
+Place the TLE files in the `sats/` directory.
+
+### Syntax
+```
+SATELLITE NAME
+TLE-LINE-1
+TLE-LINE-2
+FREQUENCY
+BANDWIDTH (Frequency range)
+```
+
+### Example
+```
+NOAA19
+1 33591U 09005A   23156.14173894  .00000208  00000-0  13708-3 0  9991
+2 33591  99.1001 201.8759 0012774 277.1213  82.8506 14.12764011738249
+137100
+42000
+```
+
+Also make sure to adapt the GROUNDSTATION_LOCATION variable to set the groundstation location. This is necessary to accurately calculate the satellite passes.
+
+## Example Output
+
+```
+pi@sdrpi:~/sat-tracker $ python tracker.py
+===== Passes calculated for: ['METEOR M2', 'NOAA19', 'NOAA15', 'ISS (ZARYA)', 'JAS 2 (FO-29)', 'NOAA18'] =====
+=== Configuring GQRX...
+M WFM 45000 RPRT 0
+L SQL -150 RPRT 0
+=== ISS (ZARYA) overpass (2023-06-07 17:52:22 - 2023-06-07 18:02:57 for 0:10:34.696568) on 145800KHz.
+F 145800000 RPRT 0
+M WFM 40000 RPRT 0
+AOS RPRT 0
+```
+
+<img src="git-assets/gqrx-noaa.png?raw=true" width="50%">
+
+## Maintainership and License
+
+If you have any suggestions for improvements or would like to contribute to the project, feel free to submit a pull request.
+This project is licensed under the MIT License[^4]. See the [LICENSE](LICENSE) file for details.
+
+Copyright (c) 2023 louis-e
+
+[^1]: https://github.com/satellogic/orbit-predictor
+
+[^2]: "A two-line element set (TLE) is a data format encoding a list of orbital elements of an Earth-orbiting object for a given point in time, the epoch." https://en.wikipedia.org/wiki/Two-line_element_set
+
+[^3]: "Gqrx is an open source software defined radio receiver (SDR) powered by the GNU Radio and the Qt graphical toolkit." https://gqrx.dk/
+
+[^4]:
+    Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+    
+    The above copyright notice, the author ("louis-e") and this permission notice shall be included in all copies or substantial portions of the Software.
+    
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ tracker.py
@@ -0,0 +1,105 @@
+from orbit_predictor.sources import EtcTLESource
+from orbit_predictor.locations import Location
+from dateutil import tz
+from datetime import datetime
+import os, time, schedule, telnetlib, subprocess
+
+PASS_WATCHDOG_INTERVAL = 5
+GQRX_HOST = '127.0.0.1'
+GQRX_PORT = 7356
+UTC_TZ = tz.tzutc()
+LOCAL_TZ = tz.tzlocal()
+GROUNDSTATION_LOCATION = Location(
+    name="Munich",
+    latitude_deg=48.137154,
+    longitude_deg=11.576124,
+    elevation_m=520,
+)
+
+def utc_to_local(utc):
+    utc_time = utc.replace(tzinfo=UTC_TZ)
+    local_time = utc_time.astimezone(LOCAL_TZ).strftime("%Y-%m-%d %H:%M:%S")
+    return local_time
+
+def gqrx_command(command):
+    try:
+        tn = telnetlib.Telnet(GQRX_HOST, GQRX_PORT, 2)
+        tn.write(command.encode('utf-8') + b'\n')
+        response = tn.read_until(b'\n')
+        tn.close()
+        print(command, response.decode('utf-8').strip())
+    except Exception as e:
+        print(f"Couldn't execute GQRC command ({e})")
+
+def append_pass_list(tle_path):
+    sat_name = open(tle_path).readline().strip()
+    sat_frequency = open(tle_path).readlines()[3].strip()
+    sat_bandwidth = open(tle_path).readlines()[4].strip()
+
+    source = EtcTLESource(filename=tle_path)
+    predictor = source.get_predictor(sat_name)
+    predicted_pass = predictor.get_next_pass(GROUNDSTATION_LOCATION)
+    pass_list.append([
+            predicted_pass.sate_id,
+            utc_to_local(predicted_pass.aos),
+            utc_to_local(predicted_pass.los),
+            str(predicted_pass.los - predicted_pass.aos),
+            sat_frequency,
+            sat_bandwidth,
+            0
+        ])
+
+
+pass_list = [] #NAME AOS LOS DURATION FREQUENCY BANDWIDTH STATUS (0 = waiting for pass, 1 = focus, 2 = done / skip)
+def pass_watchdog():
+    if len(pass_list) == 0: # First run
+        sat_names = []
+        for tle_file in os.listdir("sats/"):
+            tle_path = "sats/" + tle_file
+            sat_name = open(tle_path).readline().strip()
+            sat_names.append(sat_name)
+            append_pass_list(tle_path)
+        print(f"===== Passes calculated for: {sat_names} =====")
+        
+        print("=== Configuring GQRX...")
+        gqrx_command('M WFM 45000')
+        gqrx_command('L SQL -150')
+        return
+
+    for element in pass_list:
+        if element[6] == 0 and datetime.strptime(element[1], "%Y-%m-%d %H:%M:%S") <= datetime.now(): # Aquistion of signal (AOS)
+            print(f"=== {element[0]} overpass ({element[1]} - {element[2]} for {element[3]}) on {element[4]}KHz")
+            for passes in pass_list:
+                if passes[6] == 1:
+                    element[6] = 2
+                    print(f"Frequency recording already in progress, skipping this pass and keeping focus on current one!")
+                    return
+
+            gqrx_command(f'F {element[4]}000')
+            gqrx_command(f'M WFM {element[5]}')
+            gqrx_command('AOS')
+            element[6] = 1
+
+        elif element[6] == 1 and datetime.strptime(element[2], "%Y-%m-%d %H:%M:%S") <= datetime.now(): # Loss of signal (LOS)
+            element[6] = 2
+
+            gqrx_command('LOS')
+            if 'NOAA' in element[0]:
+                print(f"= Decoding {element[0]} pass...")
+                # ! TODO: Automatically decode NOAA pass
+                #subprocess.Popen(["/home/pi/noaa-apt/noaa-apt","input.wav","-o","output.png"])
+            
+            pass_list.remove(element)
+            for tle_file in os.listdir("sats/"):
+                tle_path = "sats/" + tle_file
+                sat_name = open(tle_path).readline().strip()
+                if element[0] == sat_name:
+                    append_pass_list(tle_path)
+
+
+scheduler = schedule.Scheduler()
+scheduler.every(PASS_WATCHDOG_INTERVAL).seconds.do(pass_watchdog)
+
+while True:
+    scheduler.run_pending()
+    time.sleep(1)
